5fd2f1c89462cd1c893716a023e77de50b3c36ad
Tiranan <tiranan.e@ku.th>
2019-05-10 06:34:57 +0700

    enough to play

--- Myspace2.py
+++ Myspace2.py
@@ -1,4 +1,5 @@
 import arcade
+import arcade.key
 from models2 import World , Me
  
 SCREEN_WIDTH = 700
@@ -21,8 +22,9 @@ class ModelSprite(arcade.Sprite):
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
- 
-        arcade.set_background_color(arcade.color.BLACK)
+
+        self.welcome = arcade.Sprite('welcome.png')
+        self.welcome.set_position(350,350)
 
         self.background = arcade.Sprite('space5-crop.png')
         #frame = 28 pixel
@@ -31,22 +33,54 @@ class SpaceGameWindow(arcade.Window):
         self.world = World(width,height)
         self.me_sprite = ModelSprite('enemy41.png',model=self.world.me)
         self.food_sprite = ModelSprite('food1.png',model=self.world.food )
+        self.enemy_sprite = ModelSprite('enemy_angryface11.png',model=self.world.enemy)
+        self.enemy_sprite.set_position(200,200)
+
+        self.enemy2_sprite = ModelSprite('enemy_yellow2.png',model=self.world.enemy2)
+        self.enemy2_sprite.set_position(200,200)
+
+
+
+        self.game_over = ModelSprite('game over1.png')
+        self.game_over.set_position(350,350)
+
+
 
     def on_key_press(self, key, key_modifiers):
         if not self.world.is_started():
              self.world.start()
         self.world.on_key_press(key, key_modifiers)
+
         
 
     def on_draw(self):
         arcade.start_render()
+
+        # self.welcome.draw()
+        
         self.background.draw()
         self.food_sprite.draw()
+
         self.me_sprite.draw()
+
+        arcade.draw_text(str(self.world.show_score) , self.width - 180, self.height - 80,
+                            arcade.color.BLACK, 20)
+
+        if self.world.score >= 0 :
+            self.enemy_sprite.draw()
+
+        if self.world.score >= 0 :
+            self.enemy2_sprite.draw()
+
+
+        if self.world.state == 3:
+            self.game_over.draw()
+            arcade.draw_text(str(f'YOUR {self.world.show_score}') , self.width - 485, self.height - 370,
+                            arcade.color.WHITE, 30)
+
 
     def update(self, delta):
         self.world.update(delta)
-        # self.me_sprite.set_position(self.world.me.x, self.world.me.y)
  
  
 if __name__ == '__main__':
--- models2.py
+++ models2.py
@@ -7,19 +7,21 @@ DIR_DOWN = 3
 DIR_LEFT = 4
 
 VELOCITY = 4
+
 DIR_OFFSET = { DIR_UP: (0,VELOCITY),
                DIR_RIGHT: (VELOCITY,0),
                DIR_DOWN: (0,-VELOCITY),
                DIR_LEFT: (-VELOCITY,0) }
 
-# class Model:
-#     def __init__(self, world, x, y, angle):
-#         self.world = world
-#         self.x = x
-#         self.y = y
+class Model:
+    def __init__(self, world, x, y, angle):
+        self.world = world
+        self.x = x
+        self.y = y
 
-#     def hit(self, other, hit_size):
-#         return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
+    def hit(self, other, hit_size):
+        return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
+
 
 class Me:
 
@@ -35,7 +37,12 @@ class Me:
         self.has_eaten = False
 
     def can_eat(self, food):
-        if food.x - 10 <= self.x <= food.x + 10 and food.y - 10 <= self.y <= food.y + 10:
+        if food.x - 30 <= self.x <= food.x + 30 and food.y - 30 <= self.y <= food.y + 30:
+            return True
+        return False
+
+    def hit_enemy(self,enemy):
+        if enemy.x - 60 <= self.x <= enemy.x + 60 and enemy.y - 60 <= self.y <= enemy.y + 60:
             return True
         return False
 
@@ -48,6 +55,49 @@ class Me:
     def update(self, delta):
         self.x += DIR_OFFSET[self.direction][0]
         self.y += DIR_OFFSET[self.direction][1]
+
+
+class Enemy:
+ 
+    def __init__(self,world,x,y):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.vx = 2
+        self.vy = 4
+ 
+ 
+    def update(self, delta):
+
+        self.x += self.vx
+        self.y += self.vy
+
+        if self.x > self.world.width - 60 or self.x < 60:
+            self.vx = -self.vx
+        if self.y > self.world.width - 58 or self.y < 58:
+            self.vy = -self.vy
+
+class Enemy2:
+ 
+    def __init__(self,world,x,y):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.vx = 7
+        self.vy = 10
+ 
+ 
+    def update(self, delta):
+
+        self.x += self.vx
+        self.y += self.vy
+
+        if self.x > self.world.width - 60 or self.x < 60:
+            self.vx = -self.vx
+        if self.y > self.world.width - 60 or self.y < 60:
+            self.vy = -self.vy
+
+
 
 class Food:
     def __init__(self, world, x, y):
@@ -76,6 +126,11 @@ class World:
         self.food = Food(self, 600, 600)
         self.food.random_location()
         self.state = World.STATE_FROZEN
+        self.enemy = Enemy(self,200,200)
+        self.enemy2 = Enemy2(self,200,200)
+
+        self.score = 0
+        self.show_score = f'SCORE: {self.score}'
 
     def start(self):
         self.state = World.STATE_STARTED
@@ -107,10 +162,22 @@ class World:
     def update(self, delta):
         if self.state in [World.STATE_FROZEN, World.STATE_DEAD]:
             return
+        if self.me.x+40 >= self.width - 30 :
+            self.die()
+        if self.me.x-40 <= 30 :
+            self.die()
+        if self.me.y+36.5 >= self.height - 30 :
+            self.die()
+        if self.me.y-36.5 <= 30 :
+            self.die()
+        
+
         if self.me.can_eat(self.food):
             self.food.random_location()
             self.me.has_eaten = True
-        self.me.update(delta)
-        
+            self.score += 10
+            self.show_score = f'SCORE: {self.score}'
 
-        
+        self.enemy.update(delta)
+        self.enemy2.update(delta)
+        self.me.update(delta)
